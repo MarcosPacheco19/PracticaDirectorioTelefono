747c517dfc69a9f34a9e8d64d9af1e71ecf5c6af
xpacheco <xpacheco@MARCOS>
2020-06-14 15:49:44 -0500

    Fina;izacion de la clase Telefono

--- DirectorioTelefono/src/main/java/ec/edu/ups/modelo/Telefono.java
+++ DirectorioTelefono/src/main/java/ec/edu/ups/modelo/Telefono.java
@@ -5,10 +5,112 @@
  */
 package ec.edu.ups.modelo;
 
+import java.util.Objects;
+
 /**
  *
  * @author xpacheco
  */
 public class Telefono {
+    
+    //Atributos
+    private int codigo;
+    private String numero;
+    private String tipo;
+    private String operadora;
+    
+    //Constructores
+    public Telefono(){
+        
+    }
+
+    public Telefono(int codigo, String numero, String tipo, String operadora) {
+        this.codigo = codigo;
+        this.numero = numero;
+        this.tipo = tipo;
+        this.operadora = operadora;
+    }
+    
+    //Getters y setters
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getOperadora() {
+        return operadora;
+    }
+
+    public void setOperadora(String operadora) {
+        this.operadora = operadora;
+    }
+    
+    
+    //Hashcode y Equals
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + this.codigo;
+        hash = 23 * hash + Objects.hashCode(this.numero);
+        hash = 23 * hash + Objects.hashCode(this.tipo);
+        hash = 23 * hash + Objects.hashCode(this.operadora);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Telefono other = (Telefono) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        if (!Objects.equals(this.numero, other.numero)) {
+            return false;
+        }
+        if (!Objects.equals(this.tipo, other.tipo)) {
+            return false;
+        }
+        if (!Objects.equals(this.operadora, other.operadora)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    //toString
+
+    @Override
+    public String toString() {
+        return "Telefono{" + "codigo=" + codigo + ", numero=" + numero + ", tipo=" + tipo + ", operadora=" + operadora + '}';
+    }
     
 }
